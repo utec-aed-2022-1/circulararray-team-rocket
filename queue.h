24a4486163925c832dcc19a5cceececffdb5241d
sanders <sanders.chancan@utec.edu.pe>
2022-03-27 22:29:42 -0500

    Queue completed

--- queue.h
+++ queue.h
@@ -2,5 +2,44 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
-    
+    public:
+    QueueArray();
+    QueueArray(int);
+    ~QueueArray();
+    void enqueue(T data);
+    T dequeue();
+    void display();
 };
+
+template <class T>
+QueueArray<T>::QueueArray(){
+    QueueArray(0);
+}
+
+template <class T>
+QueueArray<T>::QueueArray(int _capacity){
+    this->array = new T[_capacity];
+    this->capacity = _capacity;
+    this->front = this->back = -1;
+}
+
+template <class T>
+QueueArray<T>::~QueueArray(){
+    delete[] this->array;
+}
+
+template <class T>
+void QueueArray<T>::enqueue(T data){
+    this->push_back(data);
+}
+
+template <class T>
+T QueueArray<T>::dequeue(){
+    T result=this->pop_front();
+    return result;
+}
+
+template <class T>
+void QueueArray<T>::display(){
+    cout<<this->to_string(" ");
+}
